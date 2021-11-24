a2469f4ec51c73ab2a6d24f1aaca43cce8abb5d3
Kinta Lirhio <52622514+kinta-lirhio@users.noreply.github.com>
2021-11-24 15:11:43 +0300

    version 24.11.2021

---
+++ go.js
@@ -0,0 +1,13 @@
+// $(function() {
+//
+//     var header = ("#header"),
+//         header__innerH = ("#header__inner").header__innerHeight();
+//         scrollOffset = 0;
+//
+//     $(window).on("scroll",  function() {
+//
+//       scrollOffset = $(this).scrollTop;
+//
+//       console.log(scrollOffset);
+//     })
+// });
